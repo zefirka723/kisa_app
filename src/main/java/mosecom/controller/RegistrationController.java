9a52e7cc722f4282f55f509cde4d4c341d523612
zefirka723 <manglee@mail.ru>
2019-12-15 19:25:56 +0300

    Убрала лишние импорты

--- src/main/java/mosecom/controller/RegistrationController.java
+++ src/main/java/mosecom/controller/RegistrationController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.print.Doc;
 import java.io.IOException;
 
 
--- src/main/java/mosecom/service/registration/RegItemService.java
+++ src/main/java/mosecom/service/registration/RegItemService.java
@@ -1,17 +1,9 @@
 package mosecom.service.registration;
 
 import mosecom.dictionaries.DocTypes;
-import mosecom.dto.inspections.DocumentProjection;
 import mosecom.dto.inspections.RegItemProjection;
-import mosecom.model.inspections.Document;
 import mosecom.model.inspections.RegItem;
-import mosecom.model.inspections.dictionaries.Employees;
-import mosecom.model.inspections.dictionaries.OrganizationSource;
-import mosecom.model.inspections.dictionaries.RegStatus;
-import mosecom.model.inspections.dictionaries.Secrecy;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface RegItemService {
