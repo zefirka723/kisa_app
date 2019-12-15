4dbfe634d5cbf773fc3c3e1001e99528bab9e0ff
zefirka723 <manglee@mail.ru>
2019-12-15 19:19:42 +0300

    Добавила сортировку списку сотрудников

--- src/main/java/mosecom/dao/inspections/EmployeesRepository.java
+++ src/main/java/mosecom/dao/inspections/EmployeesRepository.java
@@ -4,7 +4,10 @@ import mosecom.model.inspections.dictionaries.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
+    List<Employees> findAllByOrderByNameAsc();
 }
 
