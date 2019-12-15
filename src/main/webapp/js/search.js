5f62acd5529de50b24225368503a0be3601de488
zefirka723 <manglee@mail.ru>
2019-12-15 19:29:46 +0300

    Добавила поиск по таблице регистраций

---
+++ src/main/webapp/js/search.js
@@ -0,0 +1,19 @@
+function tableSearch() {
+    var phrase = document.getElementById("searchfield");
+    var table = document.getElementById("regtable");
+    var regPhrase = new RegExp(phrase.value, 'i');
+    var flag = false;
+    for (var i = 1; i < table.rows.length; i++) {
+        flag = false;
+        for (var j = table.rows[i].cells.length - 1; j >= 0; j--) {
+            flag = regPhrase.test(table.rows[i].cells[j].innerHTML);
+            if (flag) break;
+        }
+        if (flag) {
+            table.rows[i].style.display = "";
+        } else {
+            table.rows[i].style.display = "none";
+        }
+
+    }
+}
