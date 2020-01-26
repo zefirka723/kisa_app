6ff072ad58c3c36ea4387fc236fce8a6aad3fb9e
zefirka723 <manglee@mail.ru>
2020-01-26 15:40:49 +0300

    Добавила валидацию в доках по скважине

--- src/main/webapp/js/waterhorizonvalidation.js
+++ src/main/webapp/js/waterhorizonvalidation.js
@@ -1,6 +1,46 @@
-$(document).ready(function(){
-    $('.addingButton').prop('disabled',true);
-    $('#horIdField').keyup(function(){
-        $('.addingButton').prop('disabled', this.value == "" ? true : false);
-    })
-});
+function checkHorison() {
+    if(document.getElementById("docTypeForChecks").value == 'DESCRIPTION') {
+        var h = document.getElementById("horIdField").value != "";
+        try {
+            isOk = h | (document.getElementsByName("depthsList[0].date")[0].value == '');
+            document.getElementById("horisonHint").hidden = isOk;
+            return isOk;
+
+            // if ((document.getElementsByName("depthsList[0].date")[0].value == '')) {
+            //     document.getElementById("horisonHint").hidden = false;
+            //     return false;
+            // }
+        } catch (e) {
+            document.getElementById("horisonHint").hidden = true;
+            return true;
+        }
+    }
+    else return true;
+}
+
+function checkCollar() {
+    var isDepthFilled = document.getElementById("wellDepthField").value != "";
+    if(isDepthFilled && (document.getElementById("wellCollar").value == "")) {
+        document.getElementById("collarHint").hidden = false;
+        return false;
+    }
+    else {
+        document.getElementById("collarHint").hidden = true;
+        return true;
+    }
+}
+
+function addHorisonRow() {
+    if(document.getElementById("horIdField").value != "") {
+        return WellEdit.addRow('depthRowTemplate')
+    }
+    else {
+        document.getElementById("horisonHint").hidden = false;
+    }
+}
+
+function checkValid() {
+    var isOk = checkHorison() && checkCollar();
+    document.getElementById("errorBlock").hidden = isOk;
+    document.getElementById("saveButton").disabled = !isOk;
+}
