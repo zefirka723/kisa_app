1ad34e1dcab446cf8ca5481f2898b34ec351b233
zefirka723 <manglee@mail.ru>
2020-09-07 22:07:14 +0300

    Скачивание файла со списком документов

---
+++ src/main/java/mosecom/controller/catalog/DownloadFileController.java
@@ -0,0 +1,51 @@
+package mosecom.controller.catalog;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/download")
+public class DownloadFileController {
+
+    private static final String EXTERNAL_FILE_PATH = "\\\\tech-geo\\fgi\\FUND\\CATALOG_REPORTS_TEMP";
+
+    @RequestMapping("/file/{userId}/{fileName:.+}")
+    public void downloadPDFResource(HttpServletRequest request, HttpServletResponse response,
+                                    @PathVariable("fileName") String fileName,
+                                    @PathVariable("userId") String userId) throws IOException {
+
+        File file = new File(EXTERNAL_FILE_PATH + "\\" + userId + "\\" + fileName);
+        if (file.exists()) {
+
+            //get the mimetype
+            String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+            if (mimeType == null) {
+                //unknown mimetype so set the mimetype to application/octet-stream
+                mimeType = "application/octet-stream";
+            }
+
+            response.setContentType(mimeType);
+
+            response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() + "\""));
+
+            response.setContentLength((int) file.length());
+
+            InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+
+            FileCopyUtils.copy(inputStream, response.getOutputStream());
+
+        }
+    }
+}
